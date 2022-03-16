86b5912614d2f7d829c17e26c08a37f1e699e843
student <student@mini-12.local>
2022-03-16 03:29:46 -0700

    dodanie opcji dla uzytkownika

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
-#include "trygonometria.h"
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include "trygonometria.h"    
+#else
+	#include <cmath>
+#endif
 #include <iostream>
 
 int main() { 
 
-    degreemath::sin(12);
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+std::cout<<"Using degrees\n";
+double result = degreemath::sin(45.0); 
+
+#else
+std::cout<<"Using radians\n";
+double result=std::sin(M_PI/4);
+
+#endif
+
+std::cout<<result<<std::endl;    
 
 }
